77f0019bd8881ca2f9714d6879b1ba04b40d016a
Vishal Lala <vishal.lala@gmail.com>
2022-10-11 16:54:59 -0400

    Delete fields.R

--- fields.R
+++
@@ -1,9 +0,0 @@
-# Fields Medal Winners
-
-library(rvest)
-page = read_html('https://en.wikipedia.org/wiki/Fields_Medal#Fields_medalists')
-temp = 
-  page %>%
-  html_table()
-temp[[2]]
-write.csv(temp[[2]],'fields.csv',row.names = F)
